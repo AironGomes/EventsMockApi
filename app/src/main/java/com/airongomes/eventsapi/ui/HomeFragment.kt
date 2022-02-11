0681f73f7c86c0a0ff36b6e484511dc273b6eccb
Airon Gomes <airon.gms@gmail.com>
2022-02-11 00:34:05 -0300

    adding stateflow and swipeRefresh

--- app/src/main/java/com/airongomes/eventsapi/ui/HomeFragment.kt
+++ app/src/main/java/com/airongomes/eventsapi/ui/HomeFragment.kt
@@ -1,17 +1,18 @@
 package com.airongomes.eventsapi.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Toast
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
+import com.airongomes.eventsapi.R
 import com.airongomes.eventsapi.adapter.EventAdapter
 import com.airongomes.eventsapi.base.BaseFragment
 import com.airongomes.eventsapi.databinding.FragmentHomeBinding
 import com.airongomes.eventsapi.domain.remote.NetworkResult
 import com.airongomes.eventsapi.viewModel.HomeViewModel
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
@@ -28,43 +29,41 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(FragmentHomeBinding::infl
     }
 
     private fun setupAdapter() {
+        binding.swipeRefreshLayout.setOnRefreshListener { viewModel.fetchEventList() }
         binding.rvEvents.adapter = adapter
 
-        adapter.onClick = {
-            openEventDetail(it)
-        }
-    }
-
-    private fun openEventDetail(eventId: Int) {//TODO
-        findNavController().navigate(
-            HomeFragmentDirections.openEventDetails()
-        )
+        adapter.onClick = { openEventDetail(it) }
     }
 
     private fun fetchEvents() {
         viewLifecycleOwner.lifecycleScope.launch {
-            viewModel.fetchEventList().catch { error ->
-                showErrorMessage("Unable to load data")
-            }.collectLatest {
-                when (it) {
-                    is NetworkResult.Loading -> {
-                        binding.progressbar.visibility = View.VISIBLE
-                    }
-                    is NetworkResult.Success -> {
-                        binding.progressbar.visibility = View.GONE
-                        adapter.submitList(it.data)
-                        Log.i("HomeFragment", "Events: ${it.data.toString()}")
-                    }
-                    is NetworkResult.Error -> {
-                        binding.progressbar.visibility = View.GONE
-                        showErrorMessage("Unable to load data")
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.eventList.collectLatest {
+                    when (it) {
+                        is NetworkResult.Loading -> {
+                            binding.swipeRefreshLayout.isRefreshing = true
+                        }
+                        is NetworkResult.Success -> {
+                            binding.swipeRefreshLayout.isRefreshing = false
+                            adapter.submitList(it.data)
+                        }
+                        is NetworkResult.Error -> {
+                            binding.swipeRefreshLayout.isRefreshing = false
+                            showMessage(getString(R.string.error_load_message))
+                        }
                     }
                 }
             }
         }
     }
 
-    private fun showErrorMessage(text: String) {
+    private fun showMessage(text: String) {
         Toast.makeText(requireContext(), text, Toast.LENGTH_LONG).show()
+    }
+
+    private fun openEventDetail(eventId: Int) {//TODO
+        findNavController().navigate(
+            HomeFragmentDirections.openEventDetails()
+        )
     }
 }
--- app/src/main/java/com/airongomes/eventsapi/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/airongomes/eventsapi/viewModel/HomeViewModel.kt
@@ -1,11 +1,35 @@
 package com.airongomes.eventsapi.viewModel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.airongomes.eventsapi.domain.model.Event
+import com.airongomes.eventsapi.domain.remote.NetworkResult
 import com.airongomes.eventsapi.domain.repository.EventRepository
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val repository: EventRepository,
 ): ViewModel() {
-    
-    suspend fun fetchEventList() = repository.getEventList()
+
+    init {
+        fetchEventList()
+    }
+
+    private val _eventList = MutableStateFlow<NetworkResult<List<Event>>>(NetworkResult.Loading())
+    val eventList: StateFlow<NetworkResult<List<Event>>> = _eventList
+
+    fun fetchEventList() {
+        viewModelScope.launch {
+            repository.getEventList()
+                .catch {
+                    _eventList.value = NetworkResult.Error("Unable to load data.")
+                }
+                .collect {
+                    _eventList.value = it
+                }
+        }
+    }
 }
